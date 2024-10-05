b48d169347af0c5563900cf92031516683912996
Felipe.Macedo <felipealexandrej@gmail.com>
2024-10-05 09:32:34 -0300

    feat: implement 3DES encryption and decryption using CBC mode

--- pkg/cryptoFactory/factory.go
+++ pkg/cryptoFactory/factory.go
@@ -5,28 +5,37 @@ import (
 
 	"github.com/shem-org/CryptoTool/internal/encryption/aes"
 	"github.com/shem-org/CryptoTool/internal/encryption/rsa"
+<<<<<<< HEAD
 	"github.com/shem-org/CryptoTool/internal/hash"
+=======
+	"github.com/shem-org/CryptoTool/internal/encryption/triple_des"
+	hash "github.com/shem-org/CryptoTool/internal/hash/sha256"
+>>>>>>> d57b274 (feat: implement 3DES encryption and decryption using CBC mode)
 	"github.com/shem-org/CryptoTool/internal/interfaces"
 )
 
 const (
-	AES    = "AES"
-	RSA    = "RSA"
-	SHA256 = "SHA256"
+	AES       = "AES"
+	RSA       = "RSA"
+	SHA256    = "SHA256"
+	TripleDES = "3DES"
 )
 
 func GetCrypto(algo string, bits int) (interfaces.Crypto, interface{}, interface{}, error) {
 	switch algo {
 	case AES:
-		key := make([]byte, 32) // exemple key AES-256
+		key := make([]byte, 32) // Example key for AES-256
 		return &aes.AESCrypto{}, key, nil, nil
 	case RSA:
-		// Generates and returns the RSA instance, private key and public key
+		// Generate and return RSA keys (private and public)
 		privKey, pubKey, err := rsa.GenerateRSAKeys(bits)
 		if err != nil {
 			return nil, nil, nil, err
 		}
 		return &rsa.RSACrypto{}, privKey, pubKey, nil
+	case TripleDES:
+		key := make([]byte, 24) // Example key for 3DES (24 bytes)
+		return &triple_des.TripleDESCrypto{}, key, nil, nil
 	default:
 		return nil, nil, nil, errors.New("unsupported algorithm")
 	}
