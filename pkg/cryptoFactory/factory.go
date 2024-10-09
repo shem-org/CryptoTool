c862d33ed25b6c63e24e874ed4deb992cd6eee14
Felipe.Macedo <felipealexandrej@gmail.com>
2024-10-09 09:45:52 -0300

    feat: update cryptoFactory to support ECC algorithm selection

--- pkg/cryptoFactory/factory.go
+++ pkg/cryptoFactory/factory.go
@@ -1,12 +1,14 @@
 package cryptoFactory
 
 import (
+	"crypto/elliptic"
 	"errors"
 
 	"github.com/shem-org/CryptoTool/internal/encryption/aes"
 	"github.com/shem-org/CryptoTool/internal/encryption/blowfish"
 	"github.com/shem-org/CryptoTool/internal/encryption/chacha20"
 	"github.com/shem-org/CryptoTool/internal/encryption/des"
+	"github.com/shem-org/CryptoTool/internal/encryption/ecc"
 	"github.com/shem-org/CryptoTool/internal/encryption/rsa"
 <<<<<<< HEAD
 	"github.com/shem-org/CryptoTool/internal/hash"
@@ -29,31 +31,47 @@ const (
 	Blowfish   = "Blowfish"
 	HMACSHA256 = "HMAC-SHA256"
 	HMACSHA3   = "HMAC-SHA3-256"
+	ECC        = "ECC"
 )
 
 func GetCrypto(algo string, bits int) (interfaces.Crypto, interface{}, interface{}, error) {
 	switch algo {
+
 	case AES:
 		key := make([]byte, 32) // Example key for AES-256
 		return &aes.AESCrypto{}, key, nil, nil
+
 	case RSA:
 		privKey, pubKey, err := rsa.GenerateRSAKeys(bits)
 		if err != nil {
 			return nil, nil, nil, err
 		}
 		return &rsa.RSACrypto{}, privKey, pubKey, nil
+
 	case TripleDES:
 		key := make([]byte, 24) // Example key for 3DES (24 bytes)
 		return &triple_des.TripleDESCrypto{}, key, nil, nil
+
 	case DES:
 		key := make([]byte, 8) // Example key for DES (8 bytes)
 		return &des.DESCrypto{}, key, nil, nil
+
 	case ChaCha20:
 		key := make([]byte, 32) // Key size for ChaCha20 is 256 bits (32 bytes)
 		return &chacha20.ChaCha20Crypto{}, key, nil, nil
+
 	case Blowfish:
 		key := make([]byte, 16) // Example key size for Blowfish
 		return &blowfish.BlowfishCrypto{}, key, nil, nil
+
+	case ECC:
+		curve := elliptic.P256() // Escolha da curva, pode ser P-256, P-384, etc.
+		privKey, pubKey, err := ecc.GenerateECCKeys(curve)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		return &ecc.ECCCrypto{}, privKey, pubKey, nil
+
 	default:
 		return nil, nil, nil, errors.New("unsupported algorithm")
 	}
@@ -61,8 +79,10 @@ func GetCrypto(algo string, bits int) (interfaces.Crypto, interface{}, interface
 
 func GetHashFunction(algo string) (interfaces.Hash, error) {
 	switch algo {
+
 	case SHA256:
 		return &hash.SHA256Crypto{}, nil
+
 	default:
 		return nil, errors.New("unsupported hash algorithm")
 	}
@@ -71,10 +91,13 @@ func GetHashFunction(algo string) (interfaces.Hash, error) {
 // New function for HMAC
 func GetHMACFunction(algo string) (interfaces.HMAC, error) {
 	switch algo {
+
 	case HMACSHA256:
 		return hmac.NewHMAC("SHA256")
+
 	case HMACSHA3:
 		return hmac.NewHMAC("SHA3-256")
+
 	default:
 		return nil, errors.New("unsupported HMAC algorithm")
 	}
